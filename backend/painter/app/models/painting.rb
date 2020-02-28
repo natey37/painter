9df8b34b56e2d9dc3220d3f0d90090883f7d279e
Miles Higbie <miles.higbie@gmail.com>
2020-02-28 12:28:21 -0500

    backend updates

--- backend/painter/app/models/painting.rb
+++ backend/painter/app/models/painting.rb
@@ -1,2 +1,4 @@
 class Painting < ApplicationRecord
+    belongs_to :user
+    has_many :favorites
 end
--- backend/painter/db/seeds.rb
+++ backend/painter/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all 
+Painting.destroy_all
+
+u1 = User.create(name: "Miles")
+
+p1 = Painting.create(name: "The Calling", svgInner: "hello", userId: u1.id )
+
+# " <svg x="290.33331298828125" y="273" data-id="0"><circle cx="50" cy="50" r="50" fill="red" data-color="rgb(255, 0, 0)"></circle></svg><svg x="127.33331298828125" y="72" data-id="1"><circle cx="50" cy="50" r="50" fill="red" data-color="rgb(255, 0, 0)"></circle></svg><svg x="502.33331298828125" y="84" data-id="2"><circle cx="50" cy="50" r="50" fill="red" data-color="rgb(255, 0, 0)"></circle></svg><svg x="96.90431994523124" y="112.57100695694999" data-id="3"><circle cx="290.42899304305" cy="290.42899304305" r="290.42899304305" fill="rgb(100, 200, 0)" data-color="rgb(100, 200, 0)"></circle></svg>"
